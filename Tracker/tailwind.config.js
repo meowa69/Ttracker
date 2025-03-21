1cc305cd3d9810af0ebadaa2022632fb9ccb946c
mars <rebotemartian75@gmail.com>
2025-03-21 16:28:58 +0800

    MARCH 21 - ADD MODALS, REDO COMMITTEEMANAGE PAGE

--- Tracker/tailwind.config.js
+++ Tracker/tailwind.config.js
@@ -9,16 +9,6 @@ export default {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'], // Add Poppins as a custom font
       },
-
-      animation: {
-        pulse: "pulse 1.5s infinite",
-      },
-      keyframes: {
-        pulse: {
-            "0%, 100%": { transform: "scale(1)" },
-            "50%": { transform: "scale(1.1)" },
-        },
-      },
     },
   },
   plugins: [],
